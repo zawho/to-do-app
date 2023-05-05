c344ad57af37e452d6a56d4587ce8713f736cd9a
zawho <zthooper2@gmail.com>
2023-05-05 11:07:29 +0800

    Remove UI module and add HTML

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const t=()=>({title:"",description:"",dueDate:"",priority:""}),e=document.querySelector(".main-content"),n=document.createElement("button");n.className="to-do-button",n.innerText="new",e.appendChild(n);const o=[];n.addEventListener("click",(function(){o.push(t),console.log(o)}))})();
+(()=>{"use strict";document.querySelector(".new-to-do-button")})();
