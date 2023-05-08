005e1df8a80d6d88c571887377fc5fdabc64d605
zawho <zthooper2@gmail.com>
2023-05-08 10:50:01 +0800

    Connect inputs to to do objects

--- src/index.js
+++ src/index.js
@@ -4,20 +4,23 @@ import openToDoForm from "./to-do-form";
 const newToDoButton = document.querySelector('.new-to-do-button');
 const newToDoForm = document.querySelector('.new-to-do-form');
 const titleInput = document.querySelector('.title-input');
+const descriptionInput = document.querySelector('.description-input');
+const dueDateInput = document.querySelector('.due-date-input');
+const priorityInput = document.querySelector('.priority-input');
 const addToDoButton = document.querySelector('.add-to-do-button');
 newToDoButton.addEventListener('click', openToDoForm);
 
 // Default list.
 const defaultList = [];
 
-function test(e) {
+function createToDo(e) {
     if (!(titleInput.value === '')) {
         e.preventDefault();
-        defaultList.push(toDo(titleInput.value));
+        defaultList.push(toDo(titleInput.value, descriptionInput.value, dueDateInput.value, priorityInput.checked));
         newToDoForm.style.display = 'none';
         newToDoButton.style.display = 'flex';
         console.log(defaultList);
     }
 }
 
-addToDoButton.addEventListener('click', test);
+addToDoButton.addEventListener('click', createToDo);
