1730cb9e7f452cb0508ba4dfa2d012765dde5177
zawho <zthooper2@gmail.com>
2023-05-08 11:04:11 +0800

    Fix priority checkbox display

--- src/to-do-form.js
+++ src/to-do-form.js
@@ -1,11 +1,8 @@
 const newToDoButton = document.querySelector('.new-to-do-button');
 const newToDoForm = document.querySelector('.new-to-do-form');
-const titleInput = document.querySelector('.title-input');
 
 function openToDoForm() {
-    if (!(titleInput.value === '')) {
-        titleInput.value = ''
-    }
+    newToDoForm.reset();
     newToDoButton.style.display = 'none';
     newToDoForm.style.display = 'flex';
     newToDoForm.style.flexDirection = 'column';
