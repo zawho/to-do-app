0b902306bd07fedae97ba56e479925bd8f8a568e
zawho <zthooper2@gmail.com>
2023-05-09 09:41:32 +0800

    Add due date and priority to to do display

--- src/to-do-factory.js
+++ src/to-do-factory.js
@@ -6,18 +6,33 @@ const toDo = (title, description, dueDate, priority) => ({
     title, description, dueDate, priority});
 
 
-// Display project.
+// Display to do in default project.
 function displayToDo() {
     const toDoDisplay = document.createElement('div');
     const displayTitle = document.createElement('div');
+    const displayDueDate = document.createElement('div');
+    const displayPriority = document.createElement('div');
     toDoDisplay.className = 'to-do-display';
+    toDoDisplay.style.display = 'flex';
+    toDoDisplay.style.gap = '10px';
     for (let i = 0; i < defaultList.length; i++) {
         displayTitle.innerText = defaultList[defaultList.length - 1].title;
+        if (defaultList[defaultList.length - 1].dueDate === '') {
+            displayDueDate.innerText = 'no due date';
+        } else {
+            displayDueDate.innerText = defaultList[defaultList.length - 1].dueDate;
+        }
         projectDiv.appendChild(toDoDisplay);
         toDoDisplay.appendChild(displayTitle);
+        toDoDisplay.appendChild(displayDueDate);
+        if (priorityInput.checked) {
+            displayPriority.innerText = 'important';
+            toDoDisplay.appendChild(displayPriority);
+        }
     }
 }
 
+// Create to do.
 function createToDo(e) {
     if (!(titleInput.value === '')) {
         e.preventDefault();
