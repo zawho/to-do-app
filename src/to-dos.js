7a8cf6066afb88ba10818471faba9d61fef3a9a7
zawho <zthooper2@gmail.com>
2023-06-01 10:37:33 +0800

    Add basic expanded to do func and divs

--- src/to-dos.js
+++ src/to-dos.js
@@ -6,13 +6,45 @@ import { allLists, checkForList } from "./lists";
 const toDo = (title, description, dueDate, priority, list) => ({
     title, description, dueDate, priority, list});
 
+// Create expanded to do.
+function createExpandedToDo(listVar) {
+    const expandedToDoDiv = document.createElement('div');
+    const expandedTitle = document.createElement('div');
+    const expandedDescription = document.createElement('div');
+    const expandedDueDate = document.createElement('div');
+    const expandedPriority = document.createElement('div');
+    expandedToDoDiv.className = `${listMenu.value}-expanded`.replaceAll(' ', '-');
+    expandedToDoDiv.style.display = 'none';
+    for (let i = 0; i < allLists[listVar].length; i++) {
+        expandedTitle.innerText = allLists[listVar][allLists[listVar].length - 1].title;
+        if (allLists[listVar][allLists[listVar].length - 1].dueDate === '') {
+            expandedDueDate.innerText = 'no due date';
+        } else {
+            expandedDueDate.innerText = allLists[listVar][allLists[listVar].length - 1].dueDate;
+        }
+        if (allLists[listVar][allLists[listVar].length - 1].description === '') {
+            expandedDescription.innerText = 'no description';
+        } else {
+            expandedDescription.innerText = allLists[listVar][allLists[listVar].length - 1].description;
+        }
+        projectDiv.appendChild(expandedToDoDiv);
+        expandedToDoDiv.appendChild(expandedTitle);
+        expandedToDoDiv.appendChild(expandedDescription);
+        expandedToDoDiv.appendChild(expandedDueDate);
+        if (priorityInput.checked) {
+            expandedPriority.innerText = 'important';
+            expandedToDoDiv.appendChild(expandedPriority);
+        }
+    }
+}
+
 // Display to do.
 function displayToDo(listVar) {
     const toDoDisplay = document.createElement('div');
     const displayTitle = document.createElement('div');
     const displayDueDate = document.createElement('div');
     const displayPriority = document.createElement('div');
-    toDoDisplay.className = `${listMenu.value}`;
+    toDoDisplay.className = `${listMenu.value}`.replaceAll(' ', '-');
     toDoDisplay.style.display = 'flex';
     toDoDisplay.style.gap = '10px';
     for (let i = 0; i < allLists[listVar].length; i++) {
@@ -48,6 +80,7 @@ function createToDo(e) {
         newToDoButton.style.display = 'flex';
         newListButton.style.display = 'flex';
         displayToDo(currentList);
+        createExpandedToDo(currentList);
     }
 }
 
